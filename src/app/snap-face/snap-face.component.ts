4fce68f6cefe0e02f4313208f7d22f26d0f7c757
Alweare <yanis.dumont@gmail.com>
2024-02-27 14:12:28 +0100

    Recréation avec le flag --no-standalone pour avoir le fichier app.module.ts

---
+++ src/app/snap-face/snap-face.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-snap-face',
+  templateUrl: './snap-face.component.html',
+  styleUrl: './snap-face.component.scss'
+})
+export class SnapFaceComponent {
+
+}
