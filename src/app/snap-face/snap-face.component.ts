9977eac2d24295970b5838e9e45351afbbf98362
Alweare <yanis.dumont@gmail.com>
2024-02-27 15:01:45 +0100

    création de variable et utilisation du bang. Création de ngOninit et implémentation

--- src/app/snap-face/snap-face.component.ts
+++ src/app/snap-face/snap-face.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-snap-face',
   templateUrl: './snap-face.component.html',
-  styleUrl: './snap-face.component.scss'
+  styleUrl: './snap-face.component.scss',
+  
 })
-export class SnapFaceComponent {
+export class SnapFaceComponent implements OnInit{
+  title!: string;
+  description!:string;
+  createdDate!:Date;
+  snaps!:number;
+  imageUrl!: string; 
+
+ngOnInit(){
+  this.title = 'Archibald';
+  this.description = 'Mon meilleur ami depuis toujours'
+  this.createdDate = new Date();
+  this.snaps = 6;
+  this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  
+}  
 
 }
