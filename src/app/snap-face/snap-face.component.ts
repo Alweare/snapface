b89327188330d43997dca33fede93712c91e5b06
Alweare <yanis.dumont@gmail.com>
2024-02-28 15:42:29 +0100

    Bouton like/unlike (snap, UnSnap)

--- src/app/snap-face/snap-face.component.ts
+++ src/app/snap-face/snap-face.component.ts
@@ -11,19 +11,35 @@ export class SnapFaceComponent implements OnInit{
   description!:string;
   createdDate!:Date;
   snaps!:number;
-  imageUrl!: string; 
+  imageUrl!: string;
+  txtSnap!:string;
 
 ngOnInit(){
   this.title = 'Archibald';
   this.description = 'Mon meilleur ami depuis toujours'
   this.createdDate = new Date();
-  this.snaps = 6;
+  this.snaps = 0;
   this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  this.txtSnap = "Oh snap !"
+ 
   
 }  
 
 onAddSnap(){
-  this.snaps ++;
+    this.snaps ++;
+
 }
 
+  onSnap(){
+    if (this.snaps > 0)
+    {
+      this.txtSnap = "Oh snap !"
+      this.snaps --;
+      
+    }else{
+      this.snaps++;
+      this.txtSnap = "Oops, unspap !"
+      
+    }
+  }
 }
