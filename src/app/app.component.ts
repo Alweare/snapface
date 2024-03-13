1123d5810f55d4c654e717236a79613c6d23493e
Alweare <yanis.dumont@gmail.com>
2024-03-13 17:01:26 +0100

    Retrait du constructeur qui devenait trop lourd à force d'ajouter des paramètres

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FaceSnap } from './models/face-snap.model';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -12,26 +13,29 @@ export class AppComponent implements OnInit{
   myThirdSnap!:FaceSnap;
 
   ngOnInit(){
-    this.mySnap = new FaceSnap(
-      'Archimoche',
-      'Mon ennemi',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      0
-    );
-    this.mySecSnap = new FaceSnap(
-      'Angels',
-      'Vraiment une caricature de gentil !',
-      'https://cdn.pixabay.com/photo/2021/08/16/11/32/girl-6550158_1280.png',
-      new Date(),
-      0
-    );
-    this.myThirdSnap = new FaceSnap(
-      'Yanis Ninja',
-      'Vraiment le meilleur des développeurs !',
-      'https://cdn.pixabay.com/photo/2023/09/02/14/48/ai-generated-8229038_1280.png',
-      new Date(),
-      0
-    );
+    this.mySnap = {
+      title: 'Archimoche',
+      description: 'Mon ennemi',
+      imageUrl:'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      createdDate:new Date(),
+      snaps:0,
+      location : "Paris"
+    };
+    this.mySecSnap = {
+      title: 'Angels',
+      description:'Vraiment une caricature de gentil !',
+      imageUrl:'https://cdn.pixabay.com/photo/2021/08/16/11/32/girl-6550158_1280.png',
+      createdDate:new Date(),
+      snaps:0,
+      location: "La plage"
+    };
+    this.myThirdSnap = {
+      title:'Yanis Ninja',
+      description: 'Vraiment le meilleur des développeurs !',
+      imageUrl:'https://cdn.pixabay.com/photo/2023/09/02/14/48/ai-generated-8229038_1280.png',
+      createdDate:new Date(),
+      snaps:0,
+
+    };
   }
 }
--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -1,13 +1,11 @@
 export class FaceSnap {
+	title!:string
+	description!: string
+	imageUrl!: string
+	createdDate!: Date
+	snaps!:number
+	location?: string
 
-	constructor(public title:string,
-		public description: string,
-		public imageUrl: string,
-		public createdDate: Date,
-		public snaps:number){
 
-	}
 	
-	
-
 }
